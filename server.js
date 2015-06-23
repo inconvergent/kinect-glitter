93079a2467af46e8d1f9bea03b0bff77c9e641a9
Anders Hoff <inconvergent@gmail.com>
2015-06-23 17:30:03 +0200

    test

--- server.js
+++ server.js
@@ -1,32 +1,62 @@
+
+
+
 var express = require('express');
 var path = require('path');
 var app = express();
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Spawn = require('node-spawn');
 
 var util  = require('util');
 var spawn = require('child_process').spawn;
-var ls = spawn('python', ['kinect-stream.py']);
+//var kinectStream = spawn('python', ['kinect-stream.py']);
+
+function doThing(s){
+  console.log('thing');
+  console.log(s);
+}
+
+function doError(e){
+  console.log('error');
+  console.log(e);
+}
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-io.on('connection', function(socket){
-  var n = 0;
-  console.log('connected.');
-  ls.stdout.on('data', function (gdata) {
-    s = gdata.toString();
-
-    if (n%20===0){
-      console.log(n, s);
-    }
-    
-
-    n += 1;
-
-    socket.emit('event', s);
-  });
+spawn = Spawn({
+  cmd: '/home/anders/x/kinect-glitter/kinectStream.py',
+  restarts:-1,
+  onStdout: doThing,
+  onSterr: doError
 });
+
+try{
+  spawn.start();
+}
+catch(e){
+console.log('asdf');
+}
+
+
+//io.on('connection', function(socket){
+
+  //var n = 0;
+
+  //function doEmit(d) {
+    //var s = d.toString();
+    //if (n%20===0){
+      //console.log(n, s);
+    //}
+    //n += 1;
+    //socket.emit('event', s);
+  //}
+
+  //console.log('connected.');
+
+  //kinectStream.stdout.on('data',doEmit);
+
+//});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
