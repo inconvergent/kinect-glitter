37a4ed2aa79913c4a7430261b737b59ae22d8cc0
Anders Hoff <inconvergent@gmail.com>
2015-06-23 17:52:49 +0200

    better spawn

--- server.js
+++ server.js
@@ -9,54 +9,58 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Spawn = require('node-spawn');
 
-var util  = require('util');
-var spawn = require('child_process').spawn;
-//var kinectStream = spawn('python', ['kinect-stream.py']);
-
-function doThing(s){
-  console.log('thing');
-  console.log(s);
-}
-
-function doError(e){
-  console.log('error');
-  console.log(e);
-}
-
 app.use(express.static(path.join(__dirname, 'public')));
-spawn = Spawn({
-  cmd: '/home/anders/x/kinect-glitter/kinectStream.py',
-  restarts:-1,
-  onStdout: doThing,
-  onSterr: doError
+
+var cmd = '/home/anders/x/kinect-glitter/kinectStream.py';
+var cmd = '/Users/kinect1/kinect-glitter/kinectStream.py';
+
+var kinectStream;
+
+var n = 0;
+
+
+io.on('connection', function(socket){
+
+  if (kinectStream){
+    try{
+      console.log('killing old stram ...');
+      kinectStream.kill();
+      console.log('killed.');
+      n = 0;
+    }
+    catch(e){
+      console.log('Error when killing stream.');
+    }
+  }
+
+  console.log('connected.');
+
+  function doEmit(d) {
+    var s = d.toString();
+    if (n%20===0){
+      console.log(n, s);
+    }
+    n += 1;
+    socket.emit('event', s);
+  }
+
+  function doError(e){
+    console.log('error: ');
+    console.log(e.toString());
+  }
+
+  kinectStream = Spawn({
+    cmd: cmd,
+    restarts: -1,
+    onStdout: doEmit,
+    onSterr: doError
+  });
+ 
+  console.log('spawning ...');
+  kinectStream.start();
+  console.log('spawned.');
+
 });
-
-try{
-  spawn.start();
-}
-catch(e){
-console.log('asdf');
-}
-
-
-//io.on('connection', function(socket){
-
-  //var n = 0;
-
-  //function doEmit(d) {
-    //var s = d.toString();
-    //if (n%20===0){
-      //console.log(n, s);
-    //}
-    //n += 1;
-    //socket.emit('event', s);
-  //}
-
-  //console.log('connected.');
-
-  //kinectStream.stdout.on('data',doEmit);
-
-//});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
